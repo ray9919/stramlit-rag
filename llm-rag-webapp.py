e78510a38a2a0b9cbad70ae750ccf0cf1cb2546a
ray9919 <160561785+ray9919@users.noreply.github.com>
2024-03-12 21:54:26 +0800

    Add files via upload

--- llm-rag-webapp.py
+++ llm-rag-webapp.py
@@ -17,8 +17,8 @@ def _get_session():
     from streamlit.runtime import get_instance
     from streamlit.runtime.scriptrunner import get_script_run_ctx
     runtime = get_instance()
-    session_id = get_script_run_ctx().session_id
-    session_info = runtime._session_mgr.get_session_info(session_id)
+    session_id_str = get_script_run_ctx().session_id
+    session_info = runtime._session_mgr.get_session_info(session_id_str)
     if session_info is None:
         raise RuntimeError("Couldn't get your Streamlit Session object.")
     return session_info.session.id
