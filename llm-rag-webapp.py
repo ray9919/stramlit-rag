5fe6a45dcf0baa467d4367ea282cccf759a49fc2
ray9919 <160561785+ray9919@users.noreply.github.com>
2024-03-12 21:39:52 +0800

    Add files via upload

--- llm-rag-webapp.py
+++ llm-rag-webapp.py
@@ -5,12 +5,29 @@ implements text generation and Retrieval Augmented Generation (RAG) using LLMs
 and Amazon OpenSearch as the vector database.
 """
 import base64
+import json
 
 import streamlit as st
 import requests as req
 from typing import List, Tuple, Dict
+import uuid
 
-# utility functions
+
+def _get_session():
+    from streamlit.runtime import get_instance
+    from streamlit.runtime.scriptrunner import get_script_run_ctx
+    runtime = get_instance()
+    session_id = get_script_run_ctx().session_id
+    session_info = runtime._session_mgr.get_session_info(session_id)
+    if session_info is None:
+        raise RuntimeError("Couldn't get your Streamlit Session object.")
+    return session_info.session.id
+
+
+# 生成唯一的会话 ID
+session_id = _get_session()
+
+print(f"session_id={session_id}")
 
 outputs = {}
 
@@ -27,8 +44,6 @@ MODE_VALUES: List[str] = [MODE_RAG, MODE_TEXT2TEXT]
 
 # Currently we use the flan-t5-xxl for text generation
 # and gpt-j-6b for embeddings but in future we could support more
-TEXT2TEXT_MODEL_LIST: List[str] = ["flan-t5-xxl"]
-EMBEDDINGS_MODEL_LIST: List[str] = ["gpt-j-6b"]
 
 # if running this app on a compute environment that has
 # IAM cloudformation::DescribeStacks access read the 
@@ -68,15 +83,6 @@ def get_user_input() -> str:
     return input_text
 
 
-# sidebar with options
-# with st.sidebar.expander("⚙️", expanded=False):
-#     text2text_model = st.selectbox(label='Text2Text Model', options=TEXT2TEXT_MODEL_LIST)
-#     embeddings_model = st.selectbox(label='Embeddings Model', options=EMBEDDINGS_MODEL_LIST)
-#     mode = st.selectbox(label='Mode', options=MODE_VALUES)
-
-# streamlit app layout sidebar + main panel
-# the main panel has a title, a sub header and user input textbox
-# and a text area for response and history
 st.title("特斯拉智能问答客服")
 st.subheader(
     f" Powered by AWS Bedrock with Claude3.0 and Titan Embeddings G1 - Text models for embeddings")
@@ -90,7 +96,12 @@ if user_input:
     headers: Dict = {"accept": "text/plain", "Content-Type": "text/plain"}
     output: str = None
     print(f'user_input = {user_input}')
-    base64data = base64.b64encode(user_input.encode('utf-8')).decode('utf-8')
+    payload = {
+        'user_input': user_input,
+        'session_id': session_id
+    }
+    json_string = json.dumps(payload)
+    base64data = base64.b64encode(json_string.encode('utf-8')).decode('utf-8')
     print(f'base64data = {base64data}')
     resp = req.post(api_rag_ep, headers=headers, json=base64data)
     if resp.status_code != HTTP_OK:
